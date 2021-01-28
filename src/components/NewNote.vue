7f492d02ac21ddcd329e936c3cd9720508cd7d80
keijnn <kejnnn.zxc@gmail.com>
2021-01-28 17:36:55 +0200

    '#'

---
+++ src/components/NewNote.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="new-note">
+    <h1 style="color: #402caf; font-weight: bold">Notes App</h1>
+    <h1 style="margin-top: 25px">Title</h1>
+    <div class="inputs"><input v-model="note.title" />
+      <div class="choosen">
+      </div>
+    </div>
+
+    <h1>Description</h1>
+    <textarea v-model="note.descr"></textarea>
+    <button class="btn btnPrimary" @click="addNote">Add note</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    addNote() {
+      this.$emit("addNote", this.note);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.new-note {
+  text-align: center;
+  margin-top: 10px;
+}
+.btn {
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,111 @@
+<template>
+  <!-- note list -->
+  <div class="notes">
+    <div class="note" :class="{ full: !grid }" v-for="(note, index) in notes" :key="index">
+      <div class="note-header" :class="{ full: !grid }" >
+        <p>{{ note.title }}</p>
+        <p class="chrest" style="cursor: pointer;" @click="removeNote(index)">x</p>
+      </div>
+      <div class="note-body">
+        <p>{{ note.descr }}</p>
+        <span>{{ note.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notes: {
+      type: Array,
+      required: true
+    },
+    grid: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    removeNote (index) {
+      console.log(`Note id - ${index} removed`)
+      this.$emit('remove', index)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.notes {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 40px 0;
+}
+.note {
+  min-height: 160px;
+  width: 48%;
+  padding: 18px 20px;
+  margin-bottom: 20px;
+  background-color: #ffffff;
+  box-shadow: 0 30px 30px rgba(0,0,0,.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0,0,0,.04);
+    transform: translate(0,-6px);
+    transition-delay: 0s !important;
+  }
+  &.full {
+    width: 100%;
+    text-align: center;
+    transition: all .20s;
+
+  }
+}
+.note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
+  p {
+    font-size: 22px;
+    color: #402caf;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999999;
+    &.active {
+      color: #402caf;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  &.full {
+    justify-content: center;
+    .chrest {
+      position: absolute; 
+      right: 0; 
+      padding: 25px;
+      transition: all 0.25s;
+    }
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+}
+.note-body{
+  p {
+    margin: 20px 0;
+  }
+  span {
+    font-size: 14px;
+    color: #999999;
+  }
+}
+</style>
